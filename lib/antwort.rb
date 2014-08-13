216447afc88032b88c7e1b3dd5d68af26931ebaf
Julie Ng <uxjulie@gmail.com>
2014-08-13 20:16:36 +0200

    Duh

--- lib/antwort.rb
+++ lib/antwort.rb
@@ -1,7 +1,14 @@
-require 'dotenv'
-Dotenv.load
 
-Dir.glob('./antwort/*.rb').each { |file| require file }
+# Setup load paths
+libdir = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
+# Namespace
 module Antwort
 end
+
+require 'antwort/builder'
+require 'antwort/server'
+require 'antwort/version'
+require 'dotenv'
+Dotenv.load
