a8184e496ad1647761b660e3ccade69b684f10ff
Julie Ng <uxjulie@gmail.com>
2014-08-13 16:27:14 +0200

    Dynamically mount files

--- lib/antwort.rb
+++ lib/antwort.rb
@@ -1,8 +1,29 @@
 require 'sinatra/base'
 
 class Antwort < Sinatra::Base
+  Tilt.register Tilt::ERBTemplate, 'html.erb'
+
   configure do
-    set :root, File.expand_path('../', __FILE__, '/source')
-    set :views, settings.root + '/emails'
+    set :root, File.expand_path('../../', __FILE__)
+    set :views, settings.root + '/source/emails'
   end
+
+  # puts "root: #{settings.root}"
+  # puts "views: #{settings.views}"
+
+  base_dir = '/'
+  Dir.entries(settings.views).each do |file|
+    unless file == '.' || file == '..'
+
+      route = base_dir + file.split('.').first
+      puts "route: #{route}, base_dir: #{base_dir}"
+
+      get route do
+        erb file.gsub('.html.erb', '').to_sym
+      end
+    end
+  end
+
+
+  run! if app_file == $0
 end
