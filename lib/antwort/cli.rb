24bdf3f0a43910d1e9aa1f590c8999e9229bb6bb
Tobias L. Maier <me@tobiasmaier.info>
2014-09-05 10:27:21 +0200

    Get antwort init running

--- lib/antwort/cli.rb
+++ lib/antwort/cli.rb
@@ -1,10 +1,33 @@
 require 'thor'
+require 'fileutils'
 
 module Antwort
   class CLI < Thor
-    desc 'init PROJECT_NAME', 'Initializes a new project'
+    desc 'init PROJECT_NAME', 'Initializes a new Antwort project'
     def init(project_name)
-      puts 'Initializing project...'
+      @project_name = project_name
+      copy_project
+      run_bundler
+      say "Antwort project sucessfully initialized in directory #{project_dir}"
+    end
+
+    protected
+
+    attr_reader :project_name
+
+    def copy_project
+      say 'Copying template...'
+      template_dir = File.expand_path('../../template', File.dirname(__FILE__))
+      FileUtils.copy_entry(template_dir, project_dir)
+    end
+
+    def run_bundler
+      say 'Running bundler...'
+      `cd #{project_dir} && bundle install`
+    end
+
+    def project_dir
+      "./#{project_name}"
     end
   end
 end
