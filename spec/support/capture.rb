31d53b02cfc454e600e6244bbf523be58a914512
Tobias L. Maier <me@tobiasmaier.info>
2014-09-05 19:30:53 +0200

    Add capture support file

---
+++ spec/support/capture.rb
@@ -0,0 +1,17 @@
+# Source: https://github.com/erikhuda/thor
+# License: MIT
+
+def capture(stream)
+  begin
+    stream = stream.to_s
+    eval "$#{stream} = StringIO.new"
+    yield
+    result = eval("$#{stream}").string
+  ensure
+    eval("$#{stream} = #{stream.upcase}")
+  end
+
+  result
+end
+
+alias_method :silence, :capture
