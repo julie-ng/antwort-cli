9b5a3eb7b630a632c8d6d6cef9182f193b96b6e6
Julie Ng <uxjulie@gmail.com>
2014-10-04 11:59:58 +0200

    Include CLI Upload, issue #10

--- lib/antwort.rb
+++ lib/antwort.rb
@@ -3,6 +3,7 @@ module Antwort
 end
 
 require_relative 'antwort/builder'
+require_relative 'antwort/cli/upload'
 require_relative 'antwort/cli'
 require_relative 'antwort/server/assets'
 require_relative 'antwort/server/helpers'
