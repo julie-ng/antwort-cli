35325f905755f78f7da2050da2915f40b5ecc51c
Julie Ng <uxjulie@gmail.com>
2014-10-05 23:19:12 +0200

    Split up markup helpers into own helper file

--- lib/antwort.rb
+++ lib/antwort.rb
@@ -7,6 +7,7 @@ require_relative 'antwort/cli'
 require_relative 'antwort/cli/upload'
 require_relative 'antwort/server/assets'
 require_relative 'antwort/server/helpers'
+require_relative 'antwort/server/markup'
 require_relative 'antwort/server'
 require_relative 'antwort/version'
 
