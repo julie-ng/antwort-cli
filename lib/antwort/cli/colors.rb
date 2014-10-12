487360443ec0d7f91678c49a4c7150691b2bf025
Julie Ng <uxjulie@gmail.com>
2014-10-12 11:04:35 +0200

    Add colors

---
+++ lib/antwort/cli/colors.rb
@@ -0,0 +1,15 @@
+module Antwort
+  module Colors
+    def colorize(text, color_code)
+      "\e[#{color_code}m#{text}\e[0m"
+    end
+
+    def red(text)
+      colorize(text, 31)
+    end
+
+    def green(text)
+      colorize(text, 32)
+    end
+  end
+end
