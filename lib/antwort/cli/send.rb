a695863c1d387fb8aa897defd84c74448d23a060
Julie Ng <uxjulie@gmail.com>
2015-01-06 10:52:48 +0100

    Refactor - remove inheritance from Thor class. Just include Thor::Shell instead, closes #23

--- lib/antwort/cli/send.rb
+++ lib/antwort/cli/send.rb
@@ -1,9 +1,10 @@
+require 'thor/shell'
 require 'mail'
 
 module Antwort
   class CLI
-    class Send < Thor
-      include Thor::Actions
+    class Send
+      include Thor::Shell
       attr_reader :build_id, :to, :from, :subject
 
       Mail.defaults do
@@ -24,44 +25,40 @@ module Antwort
         @subject  = options[:subject]
       end
 
-      no_commands do
+      def template_name
+        @build_id.split('-')[0...-1].join('-') # removes timestamp ID
+      end
 
-        def template_name
-          @build_id.split('-')[0...-1].join('-') # removes timestamp ID
-        end
+      def build_folder
+        "build/#{@build_id}"
+      end
 
-        def build_folder
-          "build/#{@build_id}"
-        end
+      def send
+        mail_from    = from
+        mail_to      = to
+        mail_subject = subject
+        html_body    = File.open("#{build_folder}/#{template_name}.html").read
 
-        def send
-          mail_from    = from
-          mail_to      = to
-          mail_subject = subject
-          html_body    = File.open("#{build_folder}/#{template_name}.html").read
+        mail = Mail.new do
+          from    mail_from
+          to      mail_to
+          subject mail_subject
 
-          mail = Mail.new do
-            from    mail_from
-            to      mail_to
-            subject mail_subject
-
-            text_part do
-              body 'This is plain text'
-            end
-
-            html_part do
-              content_type 'text/html; charset=UTF-8'
-              body html_body
-            end
+          text_part do
+            body 'This is plain text'
           end
 
-          if mail.deliver!
-            puts mail.inspect
-            say "Sent #{build_id} at #{Time.now.strftime('%d.%m.%Y %H:%M')}", :green
-          else
-            say "Error sending #{build_id} at #{Time.now.strftime('%d.%m.%Y %H:%M')}", :red
+          html_part do
+            content_type 'text/html; charset=UTF-8'
+            body html_body
           end
+        end
 
+        if mail.deliver!
+          puts mail.inspect
+          say "Sent #{build_id} at #{Time.now.strftime('%d.%m.%Y %H:%M')}", :green
+        else
+          say "Error sending #{build_id} at #{Time.now.strftime('%d.%m.%Y %H:%M')}", :red
         end
       end
     end
