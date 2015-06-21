cc6e657d6604f81adf8cf4aa5b7fd598743f158a
Julie Ng <uxjulie@gmail.com>
2015-06-21 17:06:46 +0200

    Re-enable some cli specs

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,48 +2,47 @@ require 'spec_helper'
 
 describe Antwort::CLI do
 
-  before do
-    allow($stdout).to receive(:write)
+  before :all do
+    Dir.chdir(fixtures_root)
   end
 
-  # describe '#upload' do
-  #   let(:output) { capture(:stdout) { subject.upload('newsletter') } }
-  #   before(:each) { allow($stdout).to receive(:write) }
+  describe '#upload' do
+    let(:output) { capture(:stdout) { subject.upload('newsletter') } }
 
-  #   before :each do
-  #     allow(subject).to receive(:upload_mail)
-  #   end
+    before :each do
+      allow($stdout).to receive(:write)
+    end
 
-  #   context 'user confirms upload' do
-  #     before :each do
-  #       allow(subject).to receive(:confirms_upload?).and_return(true)
-  #     end
+    context 'user confirms upload' do
+      before :each do
+        allow_any_instance_of(Thor::Actions).to receive(:yes?).and_return(true)
+      end
 
-  #     it 'uploads mail' do
-  #       expect(subject).to receive(:upload_mail)
-  #       output
-  #     end
+      it 'uploads mail' do
+        expect(subject).to receive(:upload)
+        output
+      end
 
-  #     it 'messages success' do
-  #       expect(output).to include('Upload complete')
-  #     end
-  #   end
+      it 'messages success' do
+        expect(output).to include('create')
+      end
+    end
 
-  #   context 'user denies upload' do
-  #     before :each do
-  #       allow(subject).to receive(:confirms_upload?).and_return(false)
-  #     end
+    context 'user denies upload' do
+      before :each do
+        allow_any_instance_of(Thor::Actions).to receive(:yes?).and_return(false)
+      end
 
-  #     it 'does not upload mail' do
-  #       expect(subject).not_to receive(:upload_mail)
-  #       output
-  #     end
+      it 'does not upload mail' do
+        expect(subject).not_to receive(:upload_mail)
+        output
+      end
 
-  #     it 'messages cancelation' do
-  #       expect(output).to include('aborted')
-  #     end
-  #   end
-  # end
+      it 'messages cancelation' do
+        expect(output).to include('aborted')
+      end
+    end
+  end
 
   # describe '#confirms_upload?' do
   #   context 'option is true' do
