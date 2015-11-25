7766ef46f22db56effba337c3a557b9890e43ea4
Julie Ng <uxjulie@gmail.com>
2015-11-25 15:36:09 +0100

    Clean up logic specs, move from partial spec to logic spec

--- spec/builder/helpers_logic_spec.rb
+++ spec/builder/helpers_logic_spec.rb
@@ -20,18 +20,20 @@ describe Antwort::LogicHelpers do
     end
   end
 
-  describe "conditionals" do
-    it "preserves if x else y"
-    it "preserves if x elsif y else z"
-  end
-
-  describe "loops" do
-    it "preserves each loops as for loops"
-  end
+  #-- Comments --
 
   describe "comments" do
     it "preserves erb comments" do
-      expect(@helper.preserve_comments('<%# foo %>')).to eq('{# foo #}')
+      h = {
+        "<%# foo %>"  => "{# foo #}",
+        "<% # foo %>" => "{# foo #}",
+        "<% #foo %>"  => "{# foo #}",
+        "<%#= foo %>" => "{#= foo #}",
+        "<%# foo bar == cat %>" => "{# foo bar == cat #}"
+      }
+      h.each do |key, value|
+        expect(@helper.preserve_comments(key)).to eq(value)
+      end
     end
 
     it "preserves comments despite leading/trailing spaces" do
@@ -39,7 +41,9 @@ describe Antwort::LogicHelpers do
     end
   end
 
-  describe "variables" do
+  #-- Variables --
+
+  describe "variable outputs" do
     it "preserves interpolated strings" do
       expect(@helper.preserve_variables('<%= "color: #{foo};" %>')).to eq('{{ "color: #{foo};" }}')
     end
@@ -50,20 +54,109 @@ describe Antwort::LogicHelpers do
         expect(@helper.preserve_variables('<%= foo[:bar][:cat] %>')).to eq('{{ foo.bar.cat }}')
       end
 
-      it "preseves sring keys" do
+      it "preseves string keys" do
         expect(@helper.preserve_variables("<%= foo['bar'] %>")).to eq('{{ foo.bar }}')
+        expect(@helper.preserve_variables("<%= foo['bar']['cat'] %>")).to eq('{{ foo.bar.cat }}')
+        expect(@helper.preserve_variables('<%= foo["bar"] %>')).to eq('{{ foo.bar }}')
+        expect(@helper.preserve_variables('<%= foo["bar"]["cat"] %>')).to eq('{{ foo.bar.cat }}')
       end
     end
   end
 
-  describe "assignemnts" do
-    it "preserves assginemnts" do
-      expect(@helper.preserve_assignments('<% foo = "bar" %>')).to eq('{% set foo = "bar" %}')
+  #-- Assignments --
+
+  describe "preserves assignments" do
+
+    describe "incl. variables" do
+      it "well-formatted code" do
+        expect(@helper.preserve_assignments("<% foo = bar %>")).to eq("{% set foo = bar %}")
+      end
+
+      it "not well-formatted code" do
+        expect(@helper.preserve_assignments("<% foo=bar %>")).to eq("{% set foo = bar %}")
+        expect(@helper.preserve_assignments("<% foo =bar %>")).to eq("{% set foo = bar %}")
+        expect(@helper.preserve_assignments("<% foo=bar %>")).to eq("{% set foo = bar %}")
+      end
     end
 
-    it "resepects memoized assignemnts" do
+    it "incl. strings" do
+      expect(@helper.preserve_assignments('<% foo = "bar" %>')).to eq('{% set foo = "bar" %}')
+      expect(@helper.preserve_assignments("<% foo = 'bar' %>")).to eq("{% set foo = 'bar' %}")
+    end
+
+    it "incl. integers" do
+      expect(@helper.preserve_assignments('<% foo = 1 %>')).to eq('{% set foo = 1 %}')
+      expect(@helper.preserve_assignments('<% foo=1 %>')).to eq('{% set foo = 1 %}')
+    end
+
+    it "incl. arrays" do
+      h = {
+        "<% foo = [1] %>" => "{% set foo = [1] %}",
+        "<% foo = [ 1 ] %>" => "{% set foo = [ 1 ] %}",
+        "<% foo = [1,2] %>" => "{% set foo = [1,2] %}",
+        "<% foo = [1, 2, 3] %>" => "{% set foo = [1, 2, 3] %}"
+      }
+      h.each do |key, value|
+        expect(@helper.preserve_assignments(key)).to eq(value)
+      end
+    end
+
+    it "incl. objects/hashes" do
+      expect(@helper.preserve_assignments('<% foo =  { key: val } %>')).to eq('{% set foo = { key: val } %}')
+    end
+
+    it "respects memoized assignemnts" do
       expect(@helper.preserve_assignments('<% foo ||= "bar" %>')).to eq('{% set foo = foo || "bar" %}')
     end
+  end
+
+  #-- Conditionals --
+
+  describe "conditionals" do
+    describe "if" do
+      it "/ end" do
+        h = {
+          '<% if foo %>do something<% end %>'           => '{% if foo %}do something{% endif %}',
+          '<%if foo%>do something<%end%>'               => '{% if foo %}do something{% endif %}',
+          '<% if foo=bar %>do something<% end %>'       => '{% if foo=bar %}do something{% endif %}',
+          '<% if foo = \'bar\' %>do something<% end %>' => '{% if foo = \'bar\' %}do something{% endif %}',
+          '<% if foo = "bar" %>do something<% end %>'   => '{% if foo = "bar" %}do something{% endif %}'
+        }
+        h.each do |key, value|
+          expect(@helper.preserve_conditionals(key)).to eq(value)
+        end
+      end
+
+      it "/ else / end" do
+        expect(@helper.preserve_conditionals('<% if foo %>bar<% else %>cat<% end %>')).to eq('{% if foo %}bar{% else %}cat{% endif %}')
+      end
+
+      it "/ elsif / else / end" do
+        expect(@helper.preserve_conditionals('<% if foo = bar %>bar<% elsif foo = cat %>cat<% else %>dog<% end %>')).to eq('{% if foo = bar %}bar{% elseif foo = cat %}cat{% else %}dog{% endif %}')
+      end
+    end
+
+    describe "unless" do
+      it "/ end"
+      it "/ else / end"
+    end
+  end
+
+  #-- Loops --
+
+  describe "loops" do
+    it "preserves each loops as for loops" do
+      h = {
+        "<% cats.each do |cat| %>" => "{% for cat in cats %}",
+        # "<% cats.each_with_index do |cat, i| %>" => "{% for cat in cats %}", # what about index?
+        "<% end %>" => "{% endfor %}"
+      }
+      h.each do |key, value|
+        expect(@helper.preserve_loops(key)).to eq(value)
+      end
+    end
+
+    it "preserves each_with_index"
   end
 
 end
