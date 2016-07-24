a55636dc138e220be5e1e923ce1f687c3f59c312
Julie Ng <uxjulie@gmail.com>
2016-07-24 21:32:23 +0200

    Add new EmailData model and spec

---
+++ lib/antwort/email/data.rb
@@ -0,0 +1,25 @@
+module Antwort
+  class EmailData
+    include Antwort::Helpers
+
+    attr_reader :name, :file, :path, :data
+
+    def initialize(opts={})
+      @name = opts[:name] || ''
+      @path = opts[:path] || "#{Dir.pwd}/data"
+      @file = opts[:file] || "#{@path}/#{@name}.yml"
+      @data = load_yaml_data
+    end
+
+    private
+
+    def load_yaml_data
+      data = {}
+      if File.file? @file
+        data = YAML.load_file(@file)
+        data = symbolize_keys! data if data
+      end
+      data
+    end
+  end
+end
---
+++ spec/email/data_spec.rb
@@ -0,0 +1,43 @@
+require "spec_helper"
+
+describe Antwort::EmailTemplate do
+  before :all do
+    @fixtures_data = "#{Dir.pwd}/spec/fixtures/data"
+    @data = Antwort::EmailData.new(name: '1-demo', path: @fixtures_data)
+  end
+
+  describe "On initialization" do
+    it "has a name attribute" do
+      expect(@data.name).to eq('1-demo')
+    end
+
+    describe "has a path attribute, which" do
+      it "can be set via constructor" do
+        expect(@data.path).to eq(@fixtures_data)
+      end
+
+      it "defaults to pwd/data" do
+        expect(Antwort::EmailData.new(name: 'foo').path).to eq("#{Dir.pwd}/data")
+      end
+    end
+
+    describe "has a file, which" do
+      it "can be set as param" do
+        a = Antwort::EmailData.new({file: '/usr/foo/bar.yml'})
+        expect(a.file).to eq('/usr/foo/bar.yml')
+      end
+
+      it "defaults to pwd/data" do
+        expect(@data.file).to eq("#{@fixtures_data}/1-demo.yml")
+      end
+    end
+
+    it "has a data attribute" do
+      result = {
+        foo: [{title: 'foo'}, {title: 'bar'}]
+      }
+      expect(@data.data).to eq(result)
+    end
+  end
+
+end
