392336c2d67e78153b0f32da622c186306b06650
Julie Ng <uxjulie@gmail.com>
2016-08-14 09:34:52 +0200

    Remove unused code from server helper

--- lib/antwort/helpers/server_helper.rb
+++ lib/antwort/helpers/server_helper.rb
@@ -5,30 +5,6 @@ module Antwort
       string.nil? ? '' : string.gsub(/([^A-Za-z0-9_\/-]+)|(--)/, '_')
     end
 
-    # def get_template_file(template_name)
-    #   # Dir.entries("your/folder").select {|f| !File.directory? f}
-    #   "#{settings.views}/emails/#{template_name}/index.html.erb"
-    # end
-
-    # def get_page_title(template_name)
-    #   get_metadata(template_name)[:title] || 'Untitled'
-    # end
-
-    # def get_metadata(template_name)
-    #   read_template(template_name)[:metadata]
-    # end
-
-    # def read_template(template_name)
-    #   file = get_template_file(template_name)
-
-    #   data = File.read(file)
-    #   md = data.match(/^(?<metadata>---\s*\n.*?\n?)^(---\s*$\n?)/m)
-    #   {
-    #     body:     (md.nil?) ? data : md.post_match,
-    #     metadata: (md.nil?) ? {} : symbolize_keys!(YAML.load(md[:metadata]))
-    #   }
-    # end
-
     def template_exists?(template_name)
       File.file? settings.templates_dir + '/' + template_name + '/index.html.erb'
     end
